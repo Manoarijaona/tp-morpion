736289eb0174e113d5faa3e9c958d0b8d0b2346e
Manoarijaona <manoa.mj@gmail.com>
2022-11-20 20:46:53 +0100

    Add function players

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ class DrawMorpion:
                       "-", "-", "-",
                       "-", "-", "-"]
 
-    def IsEmpty(self):
-        return True
-
-
-
     def CreateBoard(self):
         print(f"{self.board[0]}{self.board[1]}{self.board[2]}")
         print(f"{self.board[3]}{self.board[4]}{self.board[5]}")
         print(f"{self.board[6]}{self.board[7]}{self.board[8]}")
 
+    def playerOne(self):
+        isX = int(input("Entrez un numero entre 0 et 8: "))
+        self.board[isX] = "X"
+
+    def playerTwo(self):
+        isO = int(input("Entrez un numero entre 0 et 8: "))
+        self.board[isO] = "O"
 
 
 board = DrawMorpion()
 
-board.CreateBoard()
+for i in range(9):
+    board.CreateBoard()
+    board.playerOne()
+    board.CreateBoard()
+    board.playerTwo()
+
