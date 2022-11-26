0239e2e3728fad7cceddcd48508eb10ba3f22921
Manoarijaona <manoa.mj@gmail.com>
2022-11-26 11:36:47 +0100

    Add draw function

--- board.py
+++ board.py
@@ -1,10 +1,25 @@
+from case import Case
 from states import CaseStates
-
 
 class Board:
     def __init__(self):
         return
 
     def draw(self, caseStates: []):
-        pass
+        for i in range(3):
+            for j in range(3):
+                if caseStates[i*3 + j] == Case.isO:
+                    print('O', end='')
+                elif caseStates[i*3 + j] == Case.isX:
+                    print('X', end='')
+                else:
+                    print(' ', end='')
+                if j < 2:
+                    print('|', end='')
+            print()
+            if i < 2:
+                print('-----')
+        print()
 
+board = Board()
+board.draw([Case.isEmpty,Case.isEmpty,Case.isEmpty,Case.isEmpty,Case.isEmpty,Case.isEmpty,Case.isEmpty,Case.isEmpty,Case.isEmpty,])
